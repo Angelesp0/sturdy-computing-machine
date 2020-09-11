e8cbf164390922dd919d69cf82dab7ec0a2b08af
Angelesp0 <angel-ed-s@hotmail.com>
2020-09-11 13:12:29 -0600

    heroku3

--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/now-ui-dashboard-angular'));
-
-app.get('/*', function(req, res) {
-
+app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/now-ui-dashboard-angular/index.html'));
 });
 
