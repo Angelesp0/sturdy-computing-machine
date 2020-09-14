c00a4bb4eb9cdffc7cf42f8ba70c4c4f360d087f
Angelesp0 <angel-ed-s@hotmail.com>
2020-09-14 11:21:38 -0600

    heroku4

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/now-ui-dashboard-angular'));
+app.use(express.static(__dirname + '/dist'));
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/now-ui-dashboard-angular/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
